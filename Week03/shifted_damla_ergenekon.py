29cdeaf6423757b7f0f51392a5c083a6e30053cf
DamlaErgenekon <2541001912@ogr.cbu.edu.tr>
2026-04-19 21:55:06 +0300

    Update shifted_damla_ergenekon.py

--- Week03/shifted_damla_ergenekon.py
+++ Week03/shifted_damla_ergenekon.py
@@ -3,4 +3,4 @@ def shifted(data):
     mean = sum(data) / n
     if n % 2 != 0: median = s[n // 2]
     else: median = (s[n // 2 - 1] + s[n // 2]) / 2
-    return abs(mean - median) / mean * 100
+    return abs((mean - median) / mean) * 100
